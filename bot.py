b593ff56613f8d49ed7afe882efdb85488439be6
shlyashl <ay-ya@ya.ru>
2023-04-11 13:28:22 +0000

    add transcription

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os, glob
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
@@ -8,6 +9,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.utils.helper import HelperMode
 import openai
 
+from transcription import get_transcription
 import config as cfg
 
 
@@ -15,51 +17,59 @@ openai.api_key = cfg.oai_token
 bot = Bot(token=cfg.tg_token, timeout=None)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-
 def generate_response(prompt):
-    completion = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.6,
-    )
+    completion = openai.Completion.create(engine='text-davinci-003',
+                                          prompt=prompt,
+                                          max_tokens=1024,
+                                          n=1,
+                                          stop=None,
+                                          temperature=0.6,)
     message = completion.choices[0].text
     return message
-
 
 class Talk(StatesGroup):
     mode = HelperMode.snake_case
     ACTIVE = State()
 
-
 @dp.message_handler(commands=['help', 'start'])
 async def process_help_command(message: types.Message):
-    await message.reply(
-        '— Что бы включить бота напиши "Раджеш, проснись"\n'
-        '— Что бы выключить - "Раджеш, усни"\n'
-        '— Бот отвечает только тому кто его разбудил\n'
-        '— Попросить уснуть бота могут все'
-    )
-
+    await message.reply('— Что бы включить бота напиши "Проснись"\n'
+                        '— Что бы выключить - "Усни"\n'
+                        '— Бот отвечает только тому кто его разбудил\n'
+                        '— Попросить уснуть бота могут все')
 
 @dp.message_handler()
 async def process_help_command(message: types.Message, state: FSMContext):
-    if message.text.lower().startswith('раджеш, проснись'):
+    if message.text.lower().startswith('проснись'):
         await Talk.ACTIVE.set()
         await state.update_data(user_id=message['from']['id'])
-        await message.reply('Привет, я проснулся. Напиши "Раджеш, усни", если нужно что бы я уснул')
-
+        await message.reply('Привет, я проснулся. Напиши "Усни", если нужно что бы я уснул')
 
 @dp.message_handler(state=Talk.ACTIVE)
 async def process_help_command(message: types.Message, state: FSMContext):
     user_data = await state.get_data()
-    if message.text.lower().startswith('раджеш, усни'):
+    if message.text.lower().startswith('усни'):
         await message.reply('Пока!')
         await state.finish()
     elif user_data['user_id'] == message['from']['id']:
         await message.reply(generate_response(message.text))
+
+
+@dp.message_handler(content_types=['audio'], state=Talk.ACTIVE)
+async def transcribe(message: types.Message):
+    await message.reply('Пошел транскрибировать')
+    try:
+        file = await bot.get_file(message.audio.file_id)
+        file_name = f'transcription of {message.audio.file_name}'
+        await bot.download_file(file.file_path, file_name)
+        get_transcription(file_name)
+        with open(file_name.split(".")[0]+'.txt', 'rb') as doc:
+            await bot.send_document(message.chat.id, doc)
+    except:
+        await message.reply('Что-то пошло не так')
+    finally:
+        for filename in glob.glob(f'{file_name.split(".")[0]}*'):
+            os.remove(filename) 
 
 
 if __name__ == '__main__':
---
+++ transcription.py
@@ -0,0 +1,56 @@
+import re
+from pyannote.audio import Pipeline
+from pydub import AudioSegment
+import whisper
+
+
+def millisec(timeStr):
+  spl = timeStr.split(":")
+  s = (int)((int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2]) )* 1000)
+  return s
+
+def get_whisper_result(src):
+    model = whisper.load_model('small')
+    audio = whisper.load_audio(src)
+    options = {'language': 'ru', 'task': 'transcribe', 'fp16': False, 'word_timestamps': True}
+    result = whisper.transcribe(model, audio, **options)
+    sentences, snt, start = [], '', None
+    for segment in result['segments']:
+      for i in segment['words']:
+        snt += i['word']
+        if not start:
+          start = int(i['start']*1000)
+        if i['word'][-1] in ('.', '?', '!'):
+          sentences.append([start, snt])
+          snt, start = '', None
+    return sentences
+
+def get_transcription(src):
+  # convert to wav
+  audio = AudioSegment.from_file(src, format=src.split('.')[-1])
+  audio.export(src+'.wav', format='wav')
+  # diarization
+  pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization', 
+                                      use_auth_token='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')
+  dz = str(pipeline({'uri': 'blabal', 'audio': src+'.wav'})).split('\n')
+  wsp = get_whisper_result(src)
+  dzList = []
+  for l in dz:
+    end =  int(millisec(tuple(re.findall('[0-9]+:[0-9]+:[0-9]+\.[0-9]+', string=l))[1]))
+    name = '[Первый спикер]' if not re.findall('SPEAKER_01', string=l) else '[Второй спикер]'
+    dzList.append([end, name])
+  text = []
+  for sent in wsp:
+    for dz in dzList:
+      if dz[0] >= sent[0] and dz[1] and sent[1]:
+        text.append(str(dz[1] + sent[1]))
+        break
+  text = '\n'.join(text)
+  with open(src.split('.')[0] + '.txt', 'w') as text_file:
+    text_file.write(text)
+  return text
+
+
+if __name__ == '__main__':
+  rezult = get_transcription('speech.m4a')
+  print(rezult)
